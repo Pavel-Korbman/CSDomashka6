db309921474b70c84585ac06eb9c6d37f032c691
Pavel <pavel@b2profit.ru>
2023-11-15 17:00:39 +0300

    Решил задачу

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+// Решение для М вводимого пользователем:
+
+Console.WriteLine("Сколько чисел вы хотите ввести?");
+int lot = Convert.ToInt32 (Console.ReadLine());
+int j=0;
+for (int i = 0; i < lot; i++)
+{
+  Console.WriteLine("Введите число:");
+int number = Convert.ToInt32 (Console.ReadLine());
+if(number > 0) j++;  
+}
+Console.WriteLine(j+" Положительных чисел");
+
+// string next = "y";
+// int i=0;
+// while(next.ToLower() == "y")
+// {
+// Console.WriteLine("Введите число:");
+// int number = Convert.ToInt32 (Console.ReadLine());
+// if(number > 0) i++;
+// Console.WriteLine("Продолжить ввод? (Y - да, любой символ - нет)");
+// next = Console.ReadLine();
+// }
+
+// Console.WriteLine(i+" Положительных чисел");
